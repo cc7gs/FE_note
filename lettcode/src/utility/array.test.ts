f16bfa9813e904407e224836f27d215afe1ecaa3
cc7gs <chenorange12@gamil.com>
2019-05-12 18:29:45 +0800

    array 电话号码组合

---
+++ lettcode/src/utility/array.test.ts
@@ -0,0 +1,7 @@
+import letterCombinations from '../array/letterCombinations'
+
+describe('array',()=>{
+  it('letterCombinations: 23',()=>{
+    expect(letterCombinations('2')).toStrictEqual(['a','b','c']);
+  })
+})
