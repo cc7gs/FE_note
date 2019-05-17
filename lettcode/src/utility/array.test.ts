39f9086dc315f731160d9a2852db2a62d40275ef
cc7gs <chenorange12@gamil.com>
2019-05-17 09:26:36 +0800

    交集

--- lettcode/src/utility/array.test.ts
+++ lettcode/src/utility/array.test.ts
@@ -2,6 +2,8 @@ import letterCombinations from '../array/letterCombinations'
 import hasGroupsSize from '../array/hasGroupsSize'
 import canFlowers from '../array/canPlaceFlowers';
 import grayCode from '../array/grayCode'
+import interSection from '../array/intersection'
+
 describe('array',()=>{
   it('letterCombinations: 23',()=>{
     expect(letterCombinations('2')).toStrictEqual(['a','b','c']);
@@ -13,8 +15,11 @@ describe('array',()=>{
     expect(canFlowers([0,0],1)).toBe(true)
   });
   it('grade code:2',()=>{
-    expect(grayCode(2)).toEqual([1,2,3,0])
-  })
+    expect(grayCode(2)).toEqual([0,1,3,2])
+  });
+  it('interSection:[1,2,2,2,1]',()=>{
+    expect(interSection([1,2,2,2,1],[2,1])).toEqual([1,2])
+  });
 });
 
 
--- lettcode/src/utility/string.test.ts
+++ lettcode/src/utility/string.test.ts
@@ -1,6 +1,5 @@
 import countBinarySubstrings from '../string/countBinarySubstr'
 import reverseWords from '../string/reverseWords';
-
 describe('string', () => {
   it('countBinarySubstrs:00110011', () => {
     expect(countBinarySubstrings("00110011")).toBe(6)
