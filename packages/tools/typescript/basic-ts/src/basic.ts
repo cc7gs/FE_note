5455d6fcfe71ee72fcfc34a26140a9e6662f9f3a
cc7gs <chenorange12@gamil.com>
2021-08-30 23:19:17 +0800

    fix(tools): eslint

--- packages/tools/typescript/basic-ts/src/basic.ts
+++ packages/tools/typescript/basic-ts/src/basic.ts
@@ -1,33 +1,34 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 enum Color {
   Red,
   Green,
   Blue,
 }
-let c: Color = Color.Green;
-console.log('enum:', c); //1
+const c: Color = Color.Green;
+console.log('enum:', c); // 1
 
 enum ColorTwo {
   Red = 1,
   Green,
   Blue,
 }
-let colorName: string = ColorTwo[1];
-console.log('enum2:', colorName); //Green
+const colorName: string = ColorTwo[1];
+console.log('enum2:', colorName); // Green
 
 /**
  * 接口类型
  */
 interface LabelledValue {
   label: string;
-  number?: number; //可选属性
-  readonly size: number; //只读属性
+  number?: number; // 可选属性
+  readonly size: number; // 只读属性
   [propName: string]: any; // 索引属性签名
 }
 function printLabel(labelledObj: LabelledValue) {
   console.log(labelledObj.label);
 }
-let myobj = { size: 10, label: 'size 10 Object' };
+const myobj = { size: 10, label: 'size 10 Object' };
 printLabel(myobj);
 /**
  * 接口函数类型
@@ -35,14 +36,12 @@ printLabel(myobj);
 interface SearchFunc {
   (source: string, subString: string): boolean;
 }
-let mySearch: SearchFunc = (source, subString) => {
+const mySearch: SearchFunc = (source, subString) => {
   return source.search(subString) > -1;
 };
 // 2.2 给索引签名设置只读，防止给索引赋值
-interface ReadonlyStringArray {
-  readonly [index: number]: string;
-}
-let myArray: ReadonlyStringArray = ['cc', 'wgs'];
+type ReadonlyStringArray = Readonly<Record<number, string>>;
+const myArray: ReadonlyStringArray = ['cc', 'wgs'];
 // myArray[1]='readonly'; //error!
 /**
  * 混合类型
@@ -50,23 +49,23 @@ let myArray: ReadonlyStringArray = ['cc', 'wgs'];
 interface Counter {
   (start: number): string;
   interval: number;
-  reset(): void;
+  reset: () => void;
 }
 function getCounter(): Counter {
-  let counter = function (start: number) {} as Counter;
+  const counter = function (start: number) {} as Counter;
   counter.interval = 23;
   counter.reset = function () {
     console.log('reset call func');
   };
   return counter;
 }
-let counter = getCounter();
+const counter = getCounter();
 counter(10);
 counter.reset();
 counter.interval = 5;
 
-let a = 1;
-let b = '1';
+const a = 1;
+const b = '1';
 type Type = string | number;
 
 function Print(type: Type) {
--- packages/tools/typescript/basic-ts/src/greeter.ts
+++ packages/tools/typescript/basic-ts/src/greeter.ts
@@ -1,18 +1,17 @@
+/* eslint-disable no-console */
 /**
  *@description 初识ts,包含函数定义、类、接口使用
  */
 
-
 /**
  * @description 类型注解
- * @param person 
+ * @param person
  */
 function greeter(person: string) {
-  return `hello, ${person}`
+  return `hello, ${person}`;
 }
-let user = 'cc';
-console.log('类型注解',greeter(user));
-
+const user = 'cc';
+console.log('类型注解', greeter(user));
 
 interface Persion {
   firstName: string;
@@ -25,8 +24,8 @@ interface Persion {
 function greeterTwo(person: Persion) {
   return `Hello， ${person.firstName} ${person.lastName}`;
 }
-let userTwo = { firstName: 'wu', lastName: 'chen' };
-console.log('接口',greeterTwo(userTwo));
+const userTwo = { firstName: 'wu', lastName: 'chen' };
+console.log('接口', greeterTwo(userTwo));
 
 /**
  * 类
@@ -34,12 +33,12 @@ console.log('接口',greeterTwo(userTwo));
 class Student {
   fullName: string;
   constructor(firstName: string, lastName: string) {
-    this.fullName = firstName + '' + lastName;
+    this.fullName = `${firstName}${lastName}`;
   }
 }
 
 function greeterCls(fullName: string) {
   return `Hello，${fullName}`;
 }
-let userThree = new Student('wu', 'chen');
-console.log('class:',greeterCls(userThree.fullName));
+const userThree = new Student('wu', 'chen');
+console.log('class:', greeterCls(userThree.fullName));
